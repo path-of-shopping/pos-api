cb78c47026ba67ae6433299c8c78cccd716c643f
Pascal Boutin <pboutin@mirego.com>
2018-03-10 15:25:08 -0500

    Update data for Bestiary

--- app/services/fetchers/official_poe_trade/mods_fetcher.rb
+++ app/services/fetchers/official_poe_trade/mods_fetcher.rb
@@ -16,7 +16,19 @@ module OfficialPoeTrade
       raw_mods = JSON.parse(response.body)['result'].map { |result| result['entries'] }
       raw_mods.flatten!
 
-      raw_mods.map { |mod| { id: mod['id'], name: mod['text'], type: mod['type'] } }
+      raw_mods.map! { |mod| { id: mod['id'], name: mod['text'], type: mod['type'] } }
+
+      raw_mods.sort_by { |mod| mod[:id] }
+    end
+
+  private
+
+    def compare_mods(mod_a, mod_b)
+      type_comparison = MODS_TYPE_ORDER.index(mod_a[:type]) <=> MODS_TYPE_ORDER.index(mod_b[:type])
+
+      return type_comparison unless type_comparison == 0
+
+      mod_a[:id] <=> mod_b[:id]
     end
   end
 end
